1af42779e6d34ddc939fd0924478e98290f80dfa
neroil <edwin.haffner@heig-vd.ch>
2024-11-28 16:05:39 +0100

    main menu and base layout done

--- daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -1,11 +1,19 @@
 package ch.heigvd.iict.daa.template
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
