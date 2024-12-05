d3c0be0f37a4aba99b589042e1ccd21222fc3b77
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-12-05 15:52:23 +0100

    Ajout cache

--- daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/ImageDownloader.kt
+++ daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/ImageDownloader.kt
@@ -5,37 +5,62 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import java.io.File
 import java.io.IOException
 import java.net.URL
 
 class ImageDownloader {
 
-    companion object {
-        private val TAG = this.javaClass.simpleName
+    private val TAG = this.javaClass.simpleName
+    private var cacheDir : File;
 
-        private suspend fun downloadImage(number: Int): ByteArray? = withContext(Dispatchers.IO) {
+    constructor(cache: File) {
+        cacheDir = cache
+    }
+
+    private suspend fun putByteArrayInCache(number: Int, byteArray: ByteArray) = withContext(Dispatchers.IO) {
+        try {
+            val file = File(cacheDir, "$number.jpg")
+            file.createNewFile()
+            file.writeBytes(byteArray)
+            Log.d(TAG, "Image $number saved in cache")
+        } catch (e: IOException) {
+            Log.w(TAG, "Exception while saving image in cache", e)
+        }
+    }
+
+    private suspend fun downloadImage(number: Int): ByteArray? = withContext(Dispatchers.IO) {
+        try {
+            val url = URL("https://daa.iict.ch/images/$number.jpg")
+            val byteArray = url.readBytes()
+            putByteArrayInCache(number, byteArray)
+            return@withContext byteArray
+        } catch (e: IOException) {
+            Log.w(TAG, "Exception while downloading image", e)
+            null
+        }
+    }
+
+    private suspend fun decodeImage(bytes: ByteArray?): Bitmap? =
+        withContext(Dispatchers.Default) {
             try {
-                val url = URL("https://daa.iict.ch/images/$number.jpg")
-                url.readBytes()
+                BitmapFactory.decodeByteArray(bytes, 0, bytes?.size ?: 0)
             } catch (e: IOException) {
-                Log.w(TAG, "Exception while downloading image", e)
+                Log.w(TAG, "Exception while decoding image", e)
                 null
             }
         }
 
-        private suspend fun decodeImage(bytes: ByteArray?): Bitmap? =
-            withContext(Dispatchers.Default) {
-                try {
-                    BitmapFactory.decodeByteArray(bytes, 0, bytes?.size ?: 0)
-                } catch (e: IOException) {
-                    Log.w(TAG, "Exception while decoding image", e)
-                    null
-                }
-            }
+    suspend fun getImage(number: Int): Bitmap? {
+        val file = File(cacheDir, "$number.jpg")
+        var bytes: ByteArray
 
-        suspend fun getImage(number: Int): Bitmap? {
-            val bytes = downloadImage(number)
-            return decodeImage(bytes)
+        if (file.exists()) {
+            bytes = file.readBytes()
+        } else {
+            bytes = downloadImage(number) ?: return null
         }
+
+        return decodeImage(bytes)
     }
 }
